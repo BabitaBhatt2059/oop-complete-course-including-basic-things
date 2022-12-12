3edeba04d4d8e347246ce58525cb855d7e330c0b
Babita Bhatt <babitabhatta37@gmail.com>
2022-12-12 22:51:06 +0545

    about continue and break statement

---
+++ tut10.cpp
@@ -0,0 +1,15 @@
+#include<iostream>;
+using namespace std;
+int main(){
+    int i=0;
+    for ( i = 0; i <=40; i++)
+   {
+    
+    if(i==2){
+        continue;
+    }
+    cout<<i<<endl;
+   }
+    
+return 0;
+};
