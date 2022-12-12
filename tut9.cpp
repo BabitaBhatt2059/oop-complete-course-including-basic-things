7c135f79a5e0440df381588079c0741e8618c876
Babita Bhatt <babitabhatta37@gmail.com>
2022-12-12 21:53:59 +0545

    loop statement

---
+++ tut9.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int i=45;
+    // for (int i=0; i<15; i++)
+    // cout<<i<<endl;
+    // syntax;
+    // while(condition):
+    // {
+        // c++ statement
+   
+//    syntax for  do while loop
+do{
+    cout<<i<<endl;
+    i++;
+}
+while(i<=67);
+};
