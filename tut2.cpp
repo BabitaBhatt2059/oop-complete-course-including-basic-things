cd80ceb3178b270f3cdda6a529032be44bd40c94
pradip bhatt <84025490+pradipbhatt@users.noreply.github.com>
2022-12-12 20:55:56 +0545

    Update tut2.cpp
    run before push in gitrhub repository...

--- tut2.cpp
+++ tut2.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 class student
@@ -48,7 +47,4 @@ int main(){
     s.getdata();
     s.display();
     return 0;
-
-
->>>>>>> 378fce419593235af3d2143a6ec2bd6280adfb9b
 };
